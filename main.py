13de908997daa074b9537ac642929bd231e79663
Elliot Ball <elliotjordball@gmail.com>
2020-08-02 21:44:15 +0100

    Fix bug with coercing kwarg to snake_case from camelCase + Black formatting

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ import argparse
 import json
 import os
 import re
-from typing import Dict
+from typing import Dict, Tuple
 
 OUTPUT_DIR = os.path.join(os.getcwd(), "output")
 
 
 def banner():
-    print("""
+    print(
+        """
  ______     ______     ______   ______     ______    
 /\  ___\   /\  __ \   /\__  _\ /\  ___\   /\  == \   
 \ \ \____  \ \  __ \  \/_/\ \/ \ \  __\   \ \  __<   
  \ \_____\  \ \_\ \_\    \ \_\  \ \_____\  \ \_\ \_\ 
   \/_____/   \/_/\/_/     \/_/   \/_____/   \/_/ /_/ 
-     """)
+     """
+    )
 
 
 def cater():
@@ -24,7 +26,7 @@ def cater():
     parsed_json = get_json()
     classes_props = extract_classes_from_json(parsed_json, {})
 
-    print('About to generate the following classes: {}'.format(classes_props.keys()))
+    print("About to generate the following classes: {}".format(classes_props.keys()))
 
     for key_prop in classes_props.keys():
         build_class(key_prop, classes_props[key_prop])
@@ -44,15 +46,19 @@ def remove_output_folder():
 
 def get_json() -> Dict:
     """Parse incoming string / file and retrieve the JSON"""
-    parser = argparse.ArgumentParser(description='Generate python classes directly from JSON')
+    parser = argparse.ArgumentParser(
+        description="Generate python classes directly from JSON"
+    )
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-s', type=str, help='Provide the JSON directly via the command line')
-    group.add_argument('-f', help='The file location for the JSON to parse')
+    group.add_argument(
+        "-s", type=str, help="Provide the JSON directly via the command line"
+    )
+    group.add_argument("-f", help="The file location for the JSON to parse")
 
     args = parser.parse_args()
 
     if args.s:
-        value = json.loads(''.join(args.s))
+        value = json.loads("".join(args.s))
     elif args.f:
         if os.path.isfile(args.f):
             with open(args.f) as json_file:
@@ -60,7 +66,7 @@ def get_json() -> Dict:
         else:
             raise ValueError("Please provide a valid file that contains JSON")
     else:
-        raise ValueError('No JSON has been provided')
+        raise ValueError("No JSON has been provided")
 
     return value
 
@@ -73,11 +79,15 @@ def extract_classes_from_json(p_json: Dict, keys_props: Dict) -> Dict:
     p_json (str) -- The JSON
     keys_props (dict) -- The key is the complex objects name, value is the list of properties of that object.
     """
-    if 'TopLevelParent' not in keys_props:
-        keys_props['TopLevelParent'] = p_json
+    if "TopLevelParent" not in keys_props:
+        keys_props["TopLevelParent"] = p_json
 
     for key in p_json.keys():
-        if isinstance(p_json[key], list) and len(p_json[key]) >= 1 and isinstance(p_json[key][0], dict):
+        if (
+            isinstance(p_json[key], list)
+            and len(p_json[key]) >= 1
+            and isinstance(p_json[key][0], dict)
+        ):
             keys_props[key] = p_json[key][0]
             extract_classes_from_json(p_json[key][0], keys_props)
         elif isinstance(p_json[key], dict):
@@ -94,19 +104,19 @@ def build_class(key: str, p_dict: Dict) -> None:
     key (str) -- The JSON key that points to the parsed dict. Used for filename + class name
     p_dict (Dict) -- A dict subset of the parsed JSON, represents an object we want to build
     """
-    filename = os.path.join(OUTPUT_DIR, key.capitalize() + '.py')
+    filename = os.path.join(OUTPUT_DIR, key.capitalize() + ".py")
     classname = key.capitalize()
 
-    with open(filename, 'w') as pyfile:
-        pyfile.write('class {}:'.format(classname))
-        pyfile.write('\n')
-        pyfile.write('    def __init__(self, **kwargs):')
-        pyfile.write('\n')
+    with open(filename, "w") as pyfile:
+        pyfile.write("class {}:".format(classname))
+        pyfile.write("\n")
+        pyfile.write("    def __init__(self, **kwargs):")
+        pyfile.write("\n")
 
         for prop in p_dict.keys():
-            prop = convert(prop)
-            pyfile.write('        self.{} = kwargs.get(\'{}\', None)'.format(prop, prop))
-            pyfile.write('\n')
+            converted = convert(prop)
+            pyfile.write("        self.{} = kwargs.get('{}', None)".format(converted, prop))
+            pyfile.write("\n")
 
 
 def convert(word: str) -> str:
@@ -116,9 +126,9 @@ def convert(word: str) -> str:
     Keyword arguments:
     word (str) -- The incoming JSON property
     """
-    if len(re.findall(r'(?:[A-Z][a-z]*)+', word)) > 0:
-        s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', word)
-        return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+    if len(re.findall(r"(?:[A-Z][a-z]*)+", word)) > 0:
+        s1 = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", word)
+        return re.sub("([a-z0-9])([A-Z])", r"\1_\2", s1).lower()
     else:
         return word
 
--- test.py
+++ test.py
@@ -4,13 +4,12 @@ import os
 import json
 
 EXAMPLES_DIR = os.path.join(os.getcwd(), "examples")
-PARENT_CLASS_NAME = 'TopLevelParent'
+PARENT_CLASS_NAME = "TopLevelParent"
 
 
 class TestCater(unittest.TestCase):
-
     def test_can_convert_basic_json_to_class_prop_dict(self):
-        with open(os.path.join(EXAMPLES_DIR, 'basic.json')) as json_file:
+        with open(os.path.join(EXAMPLES_DIR, "basic.json")) as json_file:
             value = json.load(json_file)
             result = main.extract_classes_from_json(value, {})
 
@@ -20,24 +19,40 @@ class TestCater(unittest.TestCase):
                 self.assertTrue(key in result[PARENT_CLASS_NAME])
 
     def test_can_convert_single_nested_object_to_class_prop_dict(self):
-        with open(os.path.join(EXAMPLES_DIR, 'single_nested_objects.json')) as json_file:
+        with open(
+            os.path.join(EXAMPLES_DIR, "single_nested_objects.json")
+        ) as json_file:
             value = json.load(json_file)
             result = main.extract_classes_from_json(value, {})
 
             self.assertTrue(PARENT_CLASS_NAME in result.keys())
-            self.assertTrue('car' in result.keys())
+            self.assertTrue("car" in result.keys())
 
             for key in value:
                 self.assertTrue(key in result[PARENT_CLASS_NAME])
 
     def test_can_convert_multiple_nested_object_to_class_prop_dict(self):
-        with open(os.path.join(EXAMPLES_DIR, 'multiple_nested_objects.json')) as json_file:
+        with open(
+            os.path.join(EXAMPLES_DIR, "multiple_nested_objects.json")
+        ) as json_file:
             value = json.load(json_file)
             result = main.extract_classes_from_json(value, {})
 
-            expected_class_names = [PARENT_CLASS_NAME, 'filters', 'matches', 'competition',
-                                    'season', 'winner', 'score', 'fullTime', 'halfTime',
-                                    'extraTime', 'penalties', 'homeTeam', 'awayTeam']
+            expected_class_names = [
+                PARENT_CLASS_NAME,
+                "filters",
+                "matches",
+                "competition",
+                "season",
+                "winner",
+                "score",
+                "fullTime",
+                "halfTime",
+                "extraTime",
+                "penalties",
+                "homeTeam",
+                "awayTeam",
+            ]
 
             for expected_class in expected_class_names:
                 self.assertTrue(expected_class in result.keys())
@@ -46,5 +61,5 @@ class TestCater(unittest.TestCase):
                 self.assertTrue(key in result[PARENT_CLASS_NAME])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
